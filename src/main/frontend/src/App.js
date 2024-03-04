8453b60fb533aa2bf1f84b9eec66fdc2064a33b2
parkjian7 <masichyun77@hanmail.net>
2024-03-04 19:23:06 +0900

    router 설정 및 login

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,26 +1,10 @@
-import './App.css';
-import React, {useState} from 'react';
-import axios from 'axios';
+import { Outlet } from 'react-router-dom';
+
 
 function App() {
-  const onClick = async ()=>{
-    //api 통신 테스트
-    axios.get('http://158.247.197.212:9090/test')
-    .then(reponse => {
-      console.log(reponse);
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
-}
-  return (
-    <div>
-      <div>
-        <h3>Axios 테스트해보기</h3>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-    </div>
-  );
+  return <>
+      <Outlet />
+  </>
 }
 
 export default App;
