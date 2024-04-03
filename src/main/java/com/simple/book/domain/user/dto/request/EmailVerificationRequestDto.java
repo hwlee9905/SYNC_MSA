081a188c69146ae718ef6308686719859f9c38a9
str0ngmk <str0ngmk@diquest.com>
2024-04-03 17:22:46 +0900

    [swagger] 설정 추가

--- src/main/java/com/simple/book/domain/user/dto/request/EmailVerificationRequestDto.java
+++ src/main/java/com/simple/book/domain/user/dto/request/EmailVerificationRequestDto.java
@@ -9,7 +9,9 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@Schema(description = "이메일 인증 정보")
 public class EmailVerificationRequestDto {
+	@Schema(description = "인증 할 이메일", required = true)
 	private String email;
 	
 	@Schema(hidden = true)
---
+++ src/main/java/com/simple/book/global/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.simple.book.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+				.title("Hinc!").description("SNS 웹 사이트 Hinc!의 API입니다.")
+				.contact(new Contact()
+						.name("MinKyeong")
+						.url("https://github.com/str0ngMK")
+						.email("qldrhsmszz@naver.com"))
+				.contact(new Contact()
+						.name("")
+						.url("")
+						.email("")
+				);
+		return new OpenAPI().components(new Components()).info(info);
+	}
+}
