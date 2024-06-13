c5bf0833b73f94b2a52d70c53dc52b800319da05
hwlee9905 <9905hyun@naver.com>
2024-06-13 20:23:24 +0900

    [Feature] swagger DTO 설명 추가

--- src/main/java/com/simple/book/domain/member/controller/MemberController.java
+++ src/main/java/com/simple/book/domain/member/controller/MemberController.java
@@ -2,9 +2,11 @@ package com.simple.book.domain.member.controller;
 
 import com.simple.book.domain.member.dto.request.MemberMappingToProjectRequestDto;
 import com.simple.book.domain.member.dto.request.MemberMappingToTaskRequestDto;
+import com.simple.book.domain.member.entity.Member;
 import com.simple.book.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,12 +17,14 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/project/member/add")
-    public String memberAddToProject(@RequestBody MemberMappingToProjectRequestDto memberMappingToProjectRequestDto) {
-        return memberService.memberAddToProject(memberMappingToProjectRequestDto);
+    public ResponseEntity<Member> memberAddToProject(@RequestBody MemberMappingToProjectRequestDto memberMappingToProjectRequestDto) {
+        Member member = memberService.memberAddToProject(memberMappingToProjectRequestDto);
+        return ResponseEntity.ok(member);
     }
 
     @PostMapping("/task/member/add")
-    public String memberAddToTask(@RequestBody MemberMappingToTaskRequestDto memberMappingToTaskRequestDto) {
-        return memberService.memberAddToTask(memberMappingToTaskRequestDto);
+    public ResponseEntity<Member> memberAddToTask(@RequestBody MemberMappingToTaskRequestDto memberMappingToTaskRequestDto) {
+        Member member = memberService.memberAddToTask(memberMappingToTaskRequestDto);
+        return ResponseEntity.ok(member);
     }
 }
--- src/main/java/com/simple/book/domain/member/dto/request/MemberMappingToProjectRequestDto.java
+++ src/main/java/com/simple/book/domain/member/dto/request/MemberMappingToProjectRequestDto.java
@@ -1,12 +1,17 @@
 package com.simple.book.domain.member.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Schema(description = "멤버를 프로젝트에 할당하기 위한 DTO")
 public class MemberMappingToProjectRequestDto {
+    @Schema(description = "유저 로그인 아이디")
     private String userId;
+    @Schema(description = "프로젝트 아이디")
     private Long projectId;
+    @Schema(description = "해당 유저의 관리자 지정 여부")
     private Boolean isManager;
 }
