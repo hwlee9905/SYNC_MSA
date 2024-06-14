bb2859b2f7c0fa74ae1c3e7b027c3cfdaa05cdc6
str0ngmk <str0ngmk@diquest.com>
2024-06-14 10:04:11 +0900

    [알람] 스웨거 설명 추가, [검색] 엘라스틱서치 추가

--- src/main/java/com/simple/book/domain/search/controller/SearchController.java
+++ src/main/java/com/simple/book/domain/search/controller/SearchController.java
@@ -1,9 +1,6 @@
 package com.simple.book.domain.search.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.simple.book.domain.search.service.SearchService;
@@ -13,9 +10,4 @@ public class SearchController {
 
 	@Autowired
 	private SearchService searchService;
-	
-	@GetMapping("/search/test")
-	public ResponseEntity<?> testSearch(@RequestParam(name = "text") String text){
-		return ResponseEntity.ok().body(searchService.test(text));
-	}
 }
