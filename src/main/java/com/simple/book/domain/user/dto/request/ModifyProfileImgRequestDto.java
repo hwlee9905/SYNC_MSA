9361bd50aa267130b94778d55f8f8bdf74d0a7c8
str0ngmk <str0ngmk@diquest.com>
2024-07-09 16:18:20 +0900

    [modify] dto에 설명 및 규칙 추가

--- src/main/java/com/simple/book/domain/user/dto/request/ModifyProfileImgRequestDto.java
+++ src/main/java/com/simple/book/domain/user/dto/request/ModifyProfileImgRequestDto.java
@@ -2,9 +2,13 @@ package com.simple.book.domain.user.dto.request;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class ModifyProfileImgRequestDto {
-private MultipartFile profileImg;
+	@Schema(description = "변경할 프로필 사진 파일")
+	@NotBlank(message = "file은(는) 필수 입력 값입니다.")
+	private MultipartFile profileImg;
 }
