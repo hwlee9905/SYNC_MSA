a54aaf7f22d8fada5a661c0c61df6b0ace08ea29
str0ngmk <qldrhsmszz@naver.com>
2024-07-14 23:41:57 +0900

    http > https 변경

--- src/main/java/com/simple/book/domain/oauth2/SuccessHandler.java
+++ src/main/java/com/simple/book/domain/oauth2/SuccessHandler.java
@@ -49,7 +49,7 @@ public class SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
                 .path("/")
                 .sameSite("None")
                 .httpOnly(false)
-                .secure(false)
+                .secure(true)
                 .maxAge(30 * 60)
                 .build();
         return cookie;
