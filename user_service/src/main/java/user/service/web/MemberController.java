6402e3faee17cc8a56e3ae2c112da4f3a0549598
hwlee9905 <9905hyun@naver.com>
2024-08-12 23:55:49 +0900

    [Feature]
    getUsersinfo api 생성

--- user_service/src/main/java/user/service/web/MemberController.java
+++ user_service/src/main/java/user/service/web/MemberController.java
@@ -32,7 +32,6 @@ public class MemberController {
         return kafkaTaskProducerService.sendAddUserToTaskEvent(memberMappingToTaskRequestDto);
     }
     //멤버 삭제 api
-
     //담당자 삭제 api
     @Operation(summary = "업무의 담당자들을 가져오기 위한 API", description = "HOST = 129.213.161.199:31585")
     @GetMapping("/project/task/api/v1/users")
--- user_service/src/main/java/user/service/web/UserController.java
+++ user_service/src/main/java/user/service/web/UserController.java
@@ -1,5 +1,7 @@
 package user.service.web;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,16 +12,24 @@ import user.service.web.dto.request.ModifyPwdRequestDto;
 import user.service.web.dto.request.ModifyUserInfoRequestDto;
 import user.service.global.advice.SuccessResponse;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/user/api/")
 @RequiredArgsConstructor
 public class UserController {
 	private final UserService userService;
 	//.requestMatchers("/user").hasAnyAuthority("USER") USER 계정 로그인 필요
-	@GetMapping("info")
-	public ResponseEntity<SuccessResponse> getCurrentUserInfo(){
+	@Operation(summary = "현재 로그인 유저 정보를 가져오는 API", description = "HOST = 150.136.153.235:30080")
+	@GetMapping("info/v1")
+	public SuccessResponse getCurrentUserInfo(){
 		//여러 유저의 정보를 가져오도록 list로 바꿀것
-		return ResponseEntity.ok().body(userService.getUserInfo());
+		return userService.getUserInfo();
+	}
+	@Operation(summary = "유저들의 정보를 가져오는 API", description = "HOST = 150.136.153.235:30080")
+	@GetMapping("info/v2")
+	public SuccessResponse getUsersInfo(@Parameter(description = "유저 아이디 입력시 오류 발생") @RequestParam List<Long> userIds){
+		return userService.getUsersInfo(userIds);
 	}
 	@PutMapping("pwd")
 	public ResponseEntity<SuccessResponse> modifyPwd(@Valid @RequestBody ModifyPwdRequestDto body){
@@ -27,11 +37,11 @@ public class UserController {
 		UserDetails userDetails = userService.loadUserByUsername(userId);
 		return ResponseEntity.ok().body(userService.modifyPwd(body, userDetails));
 	}
-	@PutMapping("Info")
-	public ResponseEntity<SuccessResponse> modifyUserInfo(@RequestBody @Valid ModifyUserInfoRequestDto body) {
-		String userId = userService.getCurrentUserId();
-		return ResponseEntity.ok().body(userService.modifyUserInfo(body, userId));
-	}
+//	@PutMapping("Info")
+//	public ResponseEntity<SuccessResponse> modifyUserInfo(@RequestBody @Valid ModifyUserInfoRequestDto body) {
+//		String userId = userService.getCurrentUserId();
+//		return ResponseEntity.ok().body(userService.modifyUserInfo(body, userId));
+//	}
 
 //	@ResponseBody
 //	@DeleteMapping("remove")
