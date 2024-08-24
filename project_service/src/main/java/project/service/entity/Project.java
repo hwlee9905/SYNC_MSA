464aafdeae707f860a3dd37f725c818731d466bd
hwlee9905 <9905hyun@naver.com>
2024-08-24 16:21:29 +0900

    [Fix]
        @Column(name = "child_count")
        private int childCount;
        @Column(name = "child_complete_count")
        private int childCompleteCount;
    child column 추가

--- project_service/src/main/java/project/service/entity/Project.java
+++ project_service/src/main/java/project/service/entity/Project.java
@@ -20,12 +20,16 @@ public class Project{
     @Column(name = "project_id")
     private long id;
     private String title;
-    private String subTitle;
+    private String subtitle;
     private String description;
     @Column(name = "start_date")
     private Date startDate;
     @Column(name = "end_date")
     private Date endDate;
+    @Column(name = "child_count")
+    private int childCount;
+    @Column(name = "child_complete_count")
+    private int childCompleteCount;
     // Add this for the one-to-many relationship with Task
     @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Task> tasks = new ArrayList<>();
