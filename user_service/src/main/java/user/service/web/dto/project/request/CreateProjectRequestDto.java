62b07496fa227daaad987d832d501c88fb68c0d7
hwlee9905 <9905hyun@naver.com>
2024-08-26 00:31:24 +0900

    [Fix]
    createProject  api 필드 validation 변경

--- user_service/src/main/java/user/service/web/dto/project/request/CreateProjectRequestDto.java
+++ user_service/src/main/java/user/service/web/dto/project/request/CreateProjectRequestDto.java
@@ -12,14 +12,18 @@ import java.util.Date;
 @Schema(description = "프로젝트를 생성하기 위한 DTO")
 public class CreateProjectRequestDto {
     @Schema(description = "프로젝트 설명")
+    @NotBlank(message = "설명은 필수 입력 값 입니다.")
     private String description;
     @Schema(description = "프로젝트 이름")
     @NotBlank(message = "제목은 필수 입력 값 입니다.")
     private String title;
     @Schema(description = "프로젝트 부제목")
+    @NotBlank(message = "부제목은 필수 입력 값 입니다.")
     private String subTitle;
     @Schema(description = "프로젝트 시작일")
+    @NotBlank(message = "시작일은 필수 입력 값 입니다.")
     private Date startDate;
     @Schema(description = "프로젝트 종료일")
+    @NotBlank(message = "종료일은 필수 입력 값 입니다.")
     private Date endDate;
 }
