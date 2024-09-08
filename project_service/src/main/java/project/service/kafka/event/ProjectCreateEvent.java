414865a05e391871022d9ad34b5f67689c5111f0
str0ngmk <qldrhsmszz@naver.com>
2024-09-08 13:50:24 +0900

    refactor: 프로젝트 생성 코드 변경

--- project_service/src/main/java/project/service/kafka/event/ProjectCreateEvent.java
+++ project_service/src/main/java/project/service/kafka/event/ProjectCreateEvent.java
@@ -7,5 +7,6 @@ import project.service.dto.request.CreateProjectRequestDto;
 public class ProjectCreateEvent {
     private CreateProjectRequestDto projectCreateRequestDto;
     private byte[] img;
+    private String extension;
     private String userId;
 }
