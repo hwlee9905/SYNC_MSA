38b19d80395c908e9a2638f78964171e39990cea
hwlee9905 <9905hyun@naver.com>
2024-10-03 22:48:28 +0900

    [Feature]
    Controller LogAop Annotation 적용

--- user_service/src/main/java/user/service/web/ProjectController.java
+++ user_service/src/main/java/user/service/web/ProjectController.java
@@ -10,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import user.service.MemberService;
 import user.service.UserService;
+import user.service.global.advice.LogAop;
 import user.service.web.dto.project.request.*;
 import user.service.global.advice.SuccessResponse;
 import user.service.kafka.project.KafkaProjectProducerService;
@@ -26,6 +27,7 @@ public class ProjectController {
     @Operation(summary = "프로젝트를 생성하기 위한 API", description = "HOST = 150.136.153.235:30443 <br>" +
             "ValidationDetails : CreateProjectRequestDto")
     @PostMapping("/user/api/project")
+    @LogAop
     public ResponseEntity<SuccessResponse> createProject(@RequestPart("data") @Valid CreateProjectRequestDto projectCreateRequestDto, @RequestPart(value = "img", required = false) MultipartFile img) {
         String userId = userService.getCurrentUserId();
         kafkaProducerService.sendCreateProjectEvent(projectCreateRequestDto, img, userId);
@@ -35,6 +37,7 @@ public class ProjectController {
     @Operation(summary = "프로젝트를 삭제하기 위한 API", description = "HOST = 150.136.153.235:30443 <br>" +
         "ValidationDetails : DeleteProjectRequestDto")
     @DeleteMapping("/user/api/project")
+    @LogAop
     public ResponseEntity<SuccessResponse> deleteProject(@RequestBody @Valid DeleteProjectRequestDto projectDeleteRequestDto) {
         String userId = userService.getCurrentUserId();
         kafkaProducerService.sendDeleteProjectEvent(projectDeleteRequestDto, userId);
@@ -45,6 +48,7 @@ public class ProjectController {
     @Operation(summary = "프로젝트를 수정하기 위한 API", description = "HOST = 150.136.153.235:30080 <br>" +
             "ValidationDetails : UpdateProjectRequestDto")
     @PutMapping("/user/api/project")
+    @LogAop
     public ResponseEntity<SuccessResponse> updateProject(@RequestBody @Valid UpdateProjectRequestDto updateProjectRequestDto, @RequestPart(value = "img", required = false) MultipartFile img) {
         kafkaProducerService.updateProject(updateProjectRequestDto, img);
         return ResponseEntity.ok().body(SuccessResponse.builder().message("프로젝트 업데이트 이벤트 생성").build());
@@ -52,11 +56,13 @@ public class ProjectController {
     
     @Operation(summary = "프로젝트들의 정보를 가져오기 위한 API", description = "HOST = 150.136.153.235:30443")
     @GetMapping("/node2/project/api/v1")
+    @LogAop
     public void getProjects(@Parameter(description = "존재하지 않는 프로젝트 아이디 입력시 오류 발생") @RequestParam List<Long> projectIds) {
     }
     
     @Operation(summary = "유저가 속해있는 프로젝트들의 ID를 가져오기 위한 API", description = "HOST = 150.136.153.235:30443")
     @GetMapping("/project/api/v2")
+    @LogAop
     public ResponseEntity<SuccessResponse> getProjectsByUserLoginId(@Parameter(description = "존재하지 않는 로그인 아이디 입력시 오류 발생") @RequestParam(name="userId") String userId) {
         Long userEntityId = userService.getUserEntityId(userId);
         return ResponseEntity.ok(memberService.getProjectIdsByUserId(userEntityId));
