518948c09a69576a1e52a556f739f7e9160b7e31
hwlee <9905hyun@naver.com>
2025-02-17 22:48:09 +0900

    fix: filedata lombok 설정 변경

--- user_service/src/main/java/user/service/kafka/task/event/TaskUpdateEvent.java
+++ user_service/src/main/java/user/service/kafka/task/event/TaskUpdateEvent.java
@@ -3,6 +3,7 @@ package user.service.kafka.task.event;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import user.service.web.dto.task.request.UpdateTaskRequestDto;
 
 import java.util.List;
@@ -14,7 +15,9 @@ public class TaskUpdateEvent {
     private List<FileData> descriptionFiles;
     private List<FileData> deletedImages;
 
-    @Data
+
+    @Getter
+    @NoArgsConstructor
     @AllArgsConstructor
     public static class FileData {
         private String fileName;
